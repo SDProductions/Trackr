94ef5800ab6e9c1f35b67c47afe54c19a5d08180
SDBagel <35588499+SDBagel@users.noreply.github.com>
2018-06-30 20:05:10 -0700

    Remove unnecessary dependencies

--- Trackr/Main.cs
+++ Trackr/Main.cs
@@ -1,14 +1,11 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Trackr
